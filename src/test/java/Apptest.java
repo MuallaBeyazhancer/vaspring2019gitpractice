012cabc14af6b7effc998240efc87bad06eb9486
MuallaBeyazhancer <49798315+MuallaBeyazhancer@users.noreply.github.com>
2019-08-06 20:16:52 -0400

    Update Apptest.java

--- src/test/java/Apptest.java
+++ src/test/java/Apptest.java
@@ -1,6 +1,7 @@
 public class Apptest {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hi");
+        System.out.println("Hi");
        
     }
 }
