c423735b82f90d6a1956ec595037ea311edaf3c1
Mualla <beyazhancer3@gmail.com>
2019-08-06 20:56:17 -0400

    2

--- src/test/java/Apptest.java
+++ src/test/java/Apptest.java
@@ -2,7 +2,5 @@ public class Apptest {
     public static void main(String[] args) {
         System.out.println("Good Morning");
         System.out.println("Good Morning");
-       
-       
     }
 }
